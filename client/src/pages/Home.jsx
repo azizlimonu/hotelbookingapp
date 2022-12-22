6336349f09e224d0324a0a7e0448509f6d3a2f46
Aziz Limonu <azizlimonu@gmail.com>
2022-12-22 16:57:16 +0800

    add ui header & footer

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
+import React from 'react';
+import Footer from '../components/footer/Footer';
+import Header from '../components/header/Header';
+import Navbar from '../components/navbar/Navbar';
+import './home.css';
 
 const Home = () => {
   return (
     <>
-      {/* Navbar */}
-      {/* Header */}
+      <Navbar />
+      <Header />
       <div className='homeContainer'>
         {/* Featured */}
         <h1 className="homeTitle">Browse by property type</h1>
@@ -12,7 +16,7 @@ const Home = () => {
         <h1 className="homeTitle">Homes guests love</h1>
         {/* <FeaturedProperties /> */}
         {/* <MailList /> */}
-        {/* <Footer /> */}
+        <Footer />
       </div>
     </>
   )
