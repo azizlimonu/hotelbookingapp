9008aacac0bac1a5f40c90596ee89a151e300921
Aziz Limonu <azizlimonu@gmail.com>
2022-12-24 11:22:12 +0800

    add mail UI

--- client/src/components/maillist/MailList.jsx
+++ client/src/components/maillist/MailList.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from 'react';
+import './maillist.css';
 
 const MailList = () => {
   return (
-    <div>MailList</div>
+    <div className="mail">
+      <h1 className="mailTitle">
+        Save Time, Save Money !
+      </h1>
+      <span className="mailDesc">
+        Sign Up and we'll send the best deals to you
+      </span>
+      <div className="mailInputContainer">
+        <input type='text' placeholder='your email' />
+        <button>Subscribe</button>
+      </div>
+    </div>
   )
 }
 
-export default MailList
+export default MailList;
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,6 +3,7 @@ import Featured from '../components/featured/Featured';
 import FeaturedProperties from '../components/featuredproperty/FeaturedProperties';
 import Footer from '../components/footer/Footer';
 import Header from '../components/header/Header';
+import MailList from '../components/maillist/MailList';
 import Navbar from '../components/navbar/Navbar';
 import PropertyList from '../components/propertylist/PropertyList';
 import './home.css';
@@ -18,6 +19,7 @@ const Home = () => {
         <PropertyList />
         <h1 className="homeTitle">Homes guests love</h1>
         <FeaturedProperties />
+        <MailList />
         <Footer />
       </div>
     </>
