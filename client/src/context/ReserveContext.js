d07e97ec1603b271f1680252bcb1880ecaeaddf8
Aziz Limonu <azizlimonu@gmail.com>
2022-12-28 12:35:05 +0800

    add context api for reservation

---
+++ client/src/context/ReserveContext.js
@@ -0,0 +1,35 @@
+import React, { createContext, useReducer } from "react";
+
+const INITIAL_STATE = {
+  selectedRooms: [],
+};
+
+export const ReserveContext = createContext(INITIAL_STATE);
+
+const ReserveReducer = (state, action) => {
+  switch (action.type) {
+    case 'NEW_RESERVE':
+      return action.payload;
+
+    case 'RESET_RESERVE':
+      return INITIAL_STATE;
+
+    default:
+      return state;
+  }
+};
+
+export const ReserveContextProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(ReserveReducer, INITIAL_STATE);
+
+  return (
+    <ReserveContext.Provider
+      value={{
+        selectedRooms: state.selectedRooms,
+        dispatch
+      }}
+    >
+      {children}
+    </ReserveContext.Provider>
+  )
+};
--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { AuthContextProvider } from './context/AuthContext';
+import { ReserveContextProvider } from './context/ReserveContext';
 import { SearchContextProvider } from './context/SearchContext';
 import './index.css';
 
@@ -10,7 +11,9 @@ root.render(
   <React.StrictMode>
     <AuthContextProvider>
       <SearchContextProvider>
-        <App />
+        <ReserveContextProvider>
+          <App />
+        </ReserveContextProvider>
       </SearchContextProvider>
     </AuthContextProvider>
   </React.StrictMode>
